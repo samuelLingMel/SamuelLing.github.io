1bab58e260fc8727e479992a23ac9953f5cddbbb
Samuel <lingofthesam@gmail.com>
2020-06-26 06:44:37 +1000

    Updates

---
+++ precache-manifest.8a0cf70c7e34ff71b415a94bec59d72b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896b1f600bd73cd9e416db281269088f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8e5406d57dc198c789d9",
+    "url": "/portfolio/static/css/2.1a0a2c47.chunk.css"
+  },
+  {
+    "revision": "1d1dd80e3aa7a30be215",
+    "url": "/portfolio/static/css/main.91f7b54d.chunk.css"
+  },
+  {
+    "revision": "8e5406d57dc198c789d9",
+    "url": "/portfolio/static/js/2.89b1f096.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.89b1f096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d1dd80e3aa7a30be215",
+    "url": "/portfolio/static/js/main.a53fd019.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "e6a6d2f8a350079dcad97e814d3559e7",
+    "url": "/portfolio/static/media/AS.e6a6d2f8.png"
+  },
+  {
+    "revision": "3b48ae8207b07a1f9477d7e6135ca211",
+    "url": "/portfolio/static/media/KH.3b48ae82.png"
+  }
+]);
